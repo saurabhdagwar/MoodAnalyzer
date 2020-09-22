e01e9cebbe73c6907affae665b574365c5de75be
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-22 09:26:36 +0530

    [Saurabh]Added: in any mood return happy

--- src/main/java/com/Bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/Bridgelabz/MoodAnalyzer.java
@@ -3,7 +3,9 @@ package com.Bridgelabz;
 public class MoodAnalyzer {
 
     public String analyzeMood(String message) {
+     if(message.contains("Sad"))
         return "SAD";
-
+     else
+         return "HAPPY";
     }
 }
--- src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
@@ -7,8 +7,14 @@ public class MoodAnalyzerTest {
     @Test
     public void Message_containsSad_ReturnSAD() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String sadMessage = moodAnalyzer.analyzeMood("I am in Sad mood ");
+        String sadMessage = moodAnalyzer.analyzeMood("I am in Sad mood");
         Assert.assertEquals("SAD", sadMessage);
     }
 
+    @Test
+    public void Message_containsHappy_ReturnHAPPY() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String happyMessage = moodAnalyzer.analyzeMood("I am in Any mood");
+        Assert.assertEquals("HAPPY",happyMessage);
+    }
 }
