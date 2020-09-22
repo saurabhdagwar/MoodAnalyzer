31b48b4e80ac9272dd476eac4770d4b127bd2503
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-22 09:22:09 +0530

    [Saurabh]Added: mood analyzer return sad mood

--- src/main/java/com/Bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/Bridgelabz/MoodAnalyzer.java
@@ -3,9 +3,7 @@ package com.Bridgelabz;
 public class MoodAnalyzer {
 
     public String analyzeMood(String message) {
-     if(message.contains("Sad"))
         return "SAD";
-     else
-         return "HAPPY";
+
     }
 }
--- src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
@@ -7,14 +7,8 @@ public class MoodAnalyzerTest {
     @Test
     public void Message_containsSad_ReturnSAD() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String sadMessage = moodAnalyzer.analyzeMood("This is a Sad message");
+        String sadMessage = moodAnalyzer.analyzeMood("I am in Sad mood ");
         Assert.assertEquals("SAD", sadMessage);
     }
 
-    @Test
-    public void Message_containsHappy_ReturnHAPPY() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String happyMessage = moodAnalyzer.analyzeMood("This is a Happy message ");
-        Assert.assertEquals("HAPPY",happyMessage);
-    }
 }
