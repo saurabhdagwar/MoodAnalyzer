7c3543d0d62947a41c4082e3bcd9a58654d60b64
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-23 15:13:20 +0530

    [Saurabh]Refactored: sad method name

--- src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/Bridgelabz/MoodAnalyzerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givingMessage_containsSad_ReturnSAD() {
+    public void givingMessage_containsSad_shouldReturnSAD() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String sadMessage = moodAnalyzer.analyzeMood("I am in Sad mood ");
         Assert.assertEquals("SAD", sadMessage);
